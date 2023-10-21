b3d217365a7044c88787ca19db9cbfc8e35aa5be
An Duong <an.duongx@gmail.com>
2023-10-20 21:22:58 -0400

    Remove horizontal stack

--- src/components/horizontal.stack/styles.ts
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-import { withStyled } from '@app/lib/styled';
-
-export const Container = withStyled(View, () => ({
-  flexDirection: 'row',
-}));
-
-export const Label = withStyled(Text, () => ({}));

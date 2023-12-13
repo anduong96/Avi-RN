3b7e84b9b65cf4d954aa7606110dc620425170aa
An Duong <an.duongx@gmail.com>
2023-12-13 16:58:50 -0500

    Fix selectors

--- src/pages/profile/settings.card/date.format.selector.tsx
+++ src/pages/profile/settings.card/date.format.selector.tsx
@@ -24,7 +24,7 @@ export const DateFormatSelector: React.FC = () => {
 
   return (
     <Select
-      isLoading={updating || preference.loading}
+      isLoading={updating || !preference.data}
       onChange={(dateFormat) =>
         updatePreference({
           variables: {
--- src/pages/profile/settings.card/measurement.selector.tsx
+++ src/pages/profile/settings.card/measurement.selector.tsx
@@ -24,7 +24,7 @@ export const MeasurementSelector: React.FC = () => {
 
   return (
     <Select
-      isLoading={updating || preference.loading}
+      isLoading={updating || !preference.data}
       onChange={(measurement) =>
         updatePreference({
           variables: {

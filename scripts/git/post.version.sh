253c8e3ef321b6a0ca098b1aca6be2b936ce8b75
An Duong <an.duongx@gmail.com>
2024-01-02 15:05:36 -0500

    Update post version script

--- scripts/git/post.version.sh
+++ scripts/git/post.version.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
 echo "Updating ios & android file versions"
-react-native-version
 
-echo "Pushing changes to git"
-git add -A
-git commit -m \"chore(release): $npm_package_version\"
-git push --follow-tags origin HEAD
+if [ -e "package.json" ]; then
+    build_gradle_path="android/app/build.gradle"
+    ios_info_plist_path="ios/Avi/Info.plist"
+    new_version=$(node -p -e "require('./package.json').version")
+    echo "Version: $new_version"
+    sed -i '' "s/versionName \"[0-9.]*\"/versionName \"$new_version\"/" $build_gradle_path
+    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $new_version" "$ios_info_plist_path"
+
+else
+    echo "Error: package.json not found in the current directory."
+fi
+
+# echo "Pushing changes to git"
+# git add -A
+# git commit -m \"chore(release): $npm_package_version\"
+# git push --follow-tags origin HEAD

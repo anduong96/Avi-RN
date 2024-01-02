aa444ccc0a2e144c54d1f6eee458c054d2c29409
An Duong <an.duongx@gmail.com>
2024-01-02 15:20:21 -0500

    Update version

--- scripts/git/post.version.sh
+++ scripts/git/post.version.sh
@@ -13,7 +13,7 @@ else
     echo "Error: package.json not found in the current directory."
 fi
 
-# echo "Pushing changes to git"
-# git add -A
-# git commit -m \"chore(release): $npm_package_version\"
-# git push --follow-tags origin HEAD
+echo "Pushing changes to git"
+git add -A
+git commit -m \"chore\(release\): $npm_package_version\"
+git push --follow-tags origin HEAD

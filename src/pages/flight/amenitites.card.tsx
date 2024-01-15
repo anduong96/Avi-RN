f74af1c945b1ada1f1a7ec0711c9ef601a6fc429
An Duong <an.duongx@gmail.com>
2024-01-14 19:34:14 -0500

    Add amenities

--- src/pages/flight/amenitites.card.tsx
+++ src/pages/flight/amenitites.card.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 
 import { isNil } from 'lodash';
+import { useLayout } from '@react-native-community/hooks';
 
 import { Group } from '@app/components/group';
+import { useTheme } from '@app/lib/hooks/use.theme';
+import { FaIcon } from '@app/components/icons.fontawesome';
+import { AircraftAmenityType } from '@app/generated/server.gql';
 import { useAircraftAmenitiesQuery } from '@app/generated/server.gql';
 
 import { useFlight } from './context';
@@ -10,6 +14,8 @@ import { SectionTile, TileLabel } from './styles';
 
 export const AmenitiesCard: React.FC = () => {
   const flight = useFlight();
+  const theme = useTheme();
+  const layout = useLayout();
   const seats = useAircraftAmenitiesQuery({
     skip: isNil(flight.aircraftTailNumber),
     variables: {
@@ -18,15 +24,59 @@ export const AmenitiesCard: React.FC = () => {
   });
 
   const data = seats.data?.aircraftAmenities;
-
   if (isNil(data)) {
     return null;
   }
 
+  const getIcon = (type: AircraftAmenityType) => {
+    switch (type) {
+      case AircraftAmenityType.INTERNET:
+        return 'wifi';
+      case AircraftAmenityType.AC_POWER:
+        return 'plug';
+      case AircraftAmenityType.AUDIO:
+        return 'headphones';
+      case AircraftAmenityType.FOOD:
+        return 'utensils';
+      case AircraftAmenityType.VIDEO:
+        return 'tv';
+    }
+  };
+
+  const idealSize = 75;
+  const count = Math.floor(layout.width / idealSize);
+  const cellSize = Math.max(layout.width / count - theme.space.small, 0) ?? 0;
+
   return (
     <SectionTile>
       <TileLabel>Amenities</TileLabel>
-      <Group></Group>
+      <Group
+        direction="row"
+        onLayout={layout.onLayout}
+        style={{
+          flexShrink: 1,
+          flexWrap: 'wrap',
+          gap: theme.space.tiny,
+          height: '100%',
+          overflow: 'hidden',
+          width: '100%',
+        }}
+      >
+        {data.map((item) => (
+          <Group
+            isCentered
+            key={item.id}
+            style={{
+              backgroundColor: theme.pallette.grey[100],
+              borderRadius: theme.borderRadius,
+              height: cellSize,
+              width: cellSize,
+            }}
+          >
+            <FaIcon name={getIcon(item.type)} size={cellSize / 3} />
+          </Group>
+        ))}
+      </Group>
     </SectionTile>
   );
 };

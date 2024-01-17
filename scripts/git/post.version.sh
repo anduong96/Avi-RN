9d09a74b1a1352f5e0aefe3fb2fe2caa43b7dc61
An Duong <an.duongx@gmail.com>
2024-01-16 19:25:33 -0500

    Fix post version script

--- scripts/git/post.version.sh
+++ scripts/git/post.version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Updating ios & android file versions"
+new_version=$(node -p -e "require('./package.json').version")
 
 if [ -e "package.json" ]; then
     build_gradle_path="android/app/build.gradle"
     ios_info_plist_path="ios/Avi/Info.plist"
-    new_version=$(node -p -e "require('./package.json').version")
     echo "Version: $new_version"
     sed -i '' "s/versionName \"[0-9.]*\"/versionName \"$new_version\"/" $build_gradle_path
     /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $new_version" "$ios_info_plist_path"
@@ -13,7 +13,11 @@ else
     echo "Error: package.json not found in the current directory."
 fi
 
-echo "Pushing changes to git"
+commit_message="chore(release): $new_version"
+
+echo "Adding files to git"
 git add -A
-git commit -m \"chore\(release\): $npm_package_version\"
+echo "Committing changes"
+git commit -m "$commit_message"
+echo "Pushing changes"
 git push --follow-tags origin HEAD
